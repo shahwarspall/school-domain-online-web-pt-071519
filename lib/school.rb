f73a584099739e1180f25e7199beeac96317885a
Shahwar Spall <shevy.spall@gmail.com>
2019-08-02 04:00:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,12 @@ def roster
   @roster
 end 
 
-Def add_student (name,grade)
-if roster 
+def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
 
 end
